3dcbc5bf06d10f97cd564b511522ac5ad591d218
tatsuof0126 <tatsuof0126@gmail.com>
2012-10-07 21:56:35 +0900

    modify search logic

--- app/controllers/near_station_controller.rb
+++ app/controllers/near_station_controller.rb
@@ -1,13 +1,28 @@
 class NearStationController < ApplicationController
 
+  MAX_DISTANCE = 2000     #最大検索範囲
+  LAT1_DISTANCE = 110.953 #北緯0.001度あたりの距離（m）
+  LON1_DISTANCE = 90.525  #東経0.001度あたりの距離（m）
+
   def findnearest
     herelat = params[:herelat].to_f #北緯
     herelon = params[:herelon].to_f #東経
-
-    stations = Station.all
+    paramdistance = params[:distance].to_i #検索範囲
     
     neareststation = nil
-    nearestdistance = 2000
+    nearestdistance = MAX_DISTANCE
+    if paramdistance != 0 && nearestdistance > paramdistance
+      nearestdistance = paramdistance
+    end
+    
+#    stations = Station.all
+
+    search_lat = nearestdistance / LAT1_DISTANCE / 1000
+    search_lon = nearestdistance / LON1_DISTANCE / 1000
+    
+    stations = Station.find(:all, :conditions=>[ "lat between ? and ? and lon between ? and ?", 
+      herelat-search_lat, herelat+search_lat, herelon-search_lon, herelon+search_lon])
+    
     
     for station in stations do 
       distance = calcdistance(herelat, herelon, station.lat, station.lon)
@@ -21,10 +36,12 @@ class NearStationController < ApplicationController
       end
     end
     
-    p '---'
-    p nearestdistance
-    p neareststation.station_name
-    p '---'
+    if neareststation != nil
+      p '---'
+      p nearestdistance
+      p neareststation.station_name
+      p '---'
+    end
     
     render json: neareststation
   end
@@ -32,11 +49,22 @@ class NearStationController < ApplicationController
   def find
     herelat = params[:herelat].to_f #北緯
     herelon = params[:herelon].to_f #東経
-
-    stations = Station.all
+    paramdistance = params[:distance].to_i #検索範囲
+    
+    neardistance = MAX_DISTANCE
+    if paramdistance != 0 && neardistance > paramdistance
+      neardistance = paramdistance
+    end
+    
+#    stations = Station.all
+    
+    search_lat = neardistance / LAT1_DISTANCE / 1000
+    search_lon = neardistance / LON1_DISTANCE / 1000
+    
+    stations = Station.find(:all, :conditions=>[ "lat between ? and ? and lon between ? and ?", 
+      herelat-search_lat, herelat+search_lat, herelon-search_lon, herelon+search_lon])
     
     nearstations = []
-    neardistance = 2000 # 近いと見なす距離
     
     for station in stations do 
       distance = calcdistance(herelat, herelon, station.lat, station.lon)
@@ -48,6 +76,7 @@ class NearStationController < ApplicationController
       if neardistance > distance
         station.distance = distance
         nearstations << station
+        
         p station.station_name
         p station.distance
         p 'added'
@@ -58,17 +87,11 @@ class NearStationController < ApplicationController
   end
   
   def calcdistance(fromlat, fromlon, tolat, tolon)
-    # 0.001ずらしたときの距離
-    # 南北110.953(m)
-    # 東西90.525(m)
-    latdistance = 110.953
-    londistance = 90.525
-    
-    diflon = (fromlon - tolon) * londistance * 1000
-    diflat = (fromlat - tolat) * latdistance * 1000
+    diflat = (fromlat - tolat) * LAT1_DISTANCE * 1000
+    diflon = (fromlon - tolon) * LON1_DISTANCE * 1000
     
     # 三平方の定理で直線距離を計算
-    distance = Math.sqrt(diflon**2 + diflat**2)
+    distance = Math.sqrt(diflat**2 + diflon**2)
       
     return distance
   end
